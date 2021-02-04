734803b38148dd02306dfa3f82fb514273850c35
Talmaj Marinc <talmaj@gmail.com>
2021-02-04 18:20:55 +0100

    Add option to pass slice parameters in init to support older onnx operator version, update tests.

--- tests/onnx2pytorch/operations/test_slice.py
+++ tests/onnx2pytorch/operations/test_slice.py
@@ -9,43 +9,63 @@ def x():
     return torch.randn(20, 10, 5).to(torch.float32)
 
 
-def test_slice_1(x):
+@pytest.mark.parametrize("init", [True, False])
+def test_slice_1(x, init):
     starts = torch.tensor([0, 0], dtype=torch.int64)
     ends = torch.tensor([3, 10], dtype=torch.int64)
     axes = torch.tensor([0, 1], dtype=torch.int64)
     steps = torch.tensor([1, 1], dtype=torch.int64)
     y = x[0:3, 0:10]
 
-    op = Slice()
-    assert torch.equal(op(x, starts, ends, axes, steps), y)
+    if init:
+        op = Slice(axes, starts, ends, steps)
+        assert torch.equal(op(x), y)
+    else:
+        op = Slice()
+        assert torch.equal(op(x, starts, ends, axes, steps), y)
 
 
-def test_slice_2(x):
+@pytest.mark.parametrize("init", [True, False])
+def test_slice_2(x, init):
     starts = torch.tensor([1], dtype=torch.int64)
     ends = torch.tensor([1000], dtype=torch.int64)
     axes = torch.tensor([1], dtype=torch.int64)
     steps = torch.tensor([2], dtype=torch.int64)
     y = x[:, 1:1000:2]
 
-    op = Slice()
-    assert torch.equal(op(x, starts, ends, axes, steps), y)
+    if init:
+        op = Slice(axes, starts, ends, steps)
+        assert torch.equal(op(x), y)
+    else:
+        op = Slice()
+        assert torch.equal(op(x, starts, ends, axes, steps), y)
 
 
-def test_slice_default_axes(x):
+@pytest.mark.parametrize("init", [True, False])
+def test_slice_default_axes(x, init):
     starts = torch.tensor([1, 2], dtype=torch.int64)
     ends = torch.tensor([9, 5], dtype=torch.int64)
     steps = torch.tensor([1, 2], dtype=torch.int64)
     y = x[1:9, 2:5:2]
 
-    op = Slice()
-    assert torch.equal(op(x, starts, ends, steps=steps), y)
+    if init:
+        op = Slice(starts=starts, ends=ends, steps=steps)
+        assert torch.equal(op(x), y)
+    else:
+        op = Slice()
+        assert torch.equal(op(x, starts, ends, steps=steps), y)
 
 
-def test_slice_default_steps(x):
+@pytest.mark.parametrize("init", [True, False])
+def test_slice_default_steps(x, init):
     starts = torch.tensor([1], dtype=torch.int64)
     ends = torch.tensor([9], dtype=torch.int64)
     axes = torch.tensor([1], dtype=torch.int64)
     y = x[:, 1:9]
 
-    op = Slice()
-    assert torch.equal(op(x, starts, ends, axes), y)
+    if init:
+        op = Slice(axes, starts, ends)
+        assert torch.equal(op(x), y)
+    else:
+        op = Slice()
+        assert torch.equal(op(x, starts, ends, axes), y)
