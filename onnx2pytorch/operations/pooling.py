ccbf9a19c77dc0bd827ec2fcc8c36bdebe2c2cdf
Calvin McCarter <calvin@lightmatter.co>
2021-05-06 09:10:30 -0700

    bugfix and tests for GlobalAveragePool

--- onnx2pytorch/operations/pooling.py
+++ onnx2pytorch/operations/pooling.py
@@ -5,5 +5,5 @@ from torch import nn
 class GlobalAveragePool(nn.Module):
     def forward(self, input: torch.Tensor):
         spatial_shape = input.ndimension() - 2
-        dim = tuple(range(spatial_shape, spatial_shape + 2))
+        dim = tuple(range(2, spatial_shape + 2))
         return torch.mean(input, dim=dim, keepdim=True)
