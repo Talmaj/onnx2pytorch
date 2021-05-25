f5d935a3817f7c01c50ba7cd33479316505fea89
Calvin McCarter <calvin@lightmatter.co>
2021-05-25 14:11:28 -0700

    np.copy in ConstantofShape

--- onnx2pytorch/operations/constant.py
+++ onnx2pytorch/operations/constant.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from torch import nn
 
@@ -5,7 +6,7 @@ from torch import nn
 class ConstantOfShape(nn.Module):
     def __init__(self, constant):
         super().__init__()
-        self.constant = torch.from_numpy(constant)
+        self.constant = torch.from_numpy(np.copy(constant))
 
     def forward(self, shape: torch.Tensor):
         return self.constant * torch.ones(*shape)
