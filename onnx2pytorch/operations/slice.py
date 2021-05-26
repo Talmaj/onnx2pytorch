63d5289cdc505c13ff079e8608ec7baa68b6d787
Calvin McCarter <calvin@lightmatter.co>
2021-05-25 22:00:34 -0700

    Slice handles negative steps not implemented in pytorch

--- onnx2pytorch/operations/slice.py
+++ onnx2pytorch/operations/slice.py
@@ -2,6 +2,26 @@ import torch
 from torch import nn
 
 
+def _to_positive_step(orig_slice, N):
+    # Get rid of backward slices
+    start, stop, step = orig_slice.indices(N)
+
+    # Get number of steps and remainder
+    n, r = divmod(stop - start, step)
+    if n < 0 or (n == 0 and r == 0):
+        return slice(0, 0, 1)
+    if r != 0:  # a "stop" index, not a last index
+        n += 1
+
+    if step < 0:
+        start, stop, step = start + (n - 1) * step, start - step, -step
+    else:  # step > 0, step == 0 is not allowed
+        stop = start + n * step
+    stop = min(stop, N)
+
+    return slice(start, stop, step)
+
+
 class Slice(nn.Module):
     def __init__(self, dim=None, starts=None, ends=None, steps=None):
         self.dim = [dim] if isinstance(dim, int) else dim
@@ -31,10 +51,17 @@ class Slice(nn.Module):
         axes = [input.ndim + x if x < 0 else x for x in axes]
 
         selection = [slice(None) for _ in range(max(axes) + 1)]
+
+        flip_dims = []
         for i, axis in enumerate(axes):
-            selection[axis] = slice(
+            raw_slice = slice(
                 starts[i].to(dtype=torch.long, device=input.device),
                 ends[i].to(dtype=torch.long, device=input.device),
                 steps[i].to(dtype=torch.long, device=input.device),
             )
-        return input.__getitem__(selection)
+            if steps[i] < 0:
+                selection[axis] = _to_positive_step(raw_slice, input.shape[axis])
+                flip_dims.append(axis)
+            else:
+                selection[axis] = raw_slice
+        return torch.flip(input.__getitem__(selection), flip_dims)
