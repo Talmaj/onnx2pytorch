2ea8d85d0cb598758aa3d1ee0e68179702135ff0
Calvin McCarter <calvin@lightmatter.co>
2021-06-01 21:04:52 -0700

    Expand- cast shape to int64

--- onnx2pytorch/operations/expand.py
+++ onnx2pytorch/operations/expand.py
@@ -4,6 +4,8 @@ from torch import nn
 
 class Expand(nn.Module):
     def forward(self, input: torch.Tensor, shape: torch.Tensor):
+        if isinstance(shape, torch.Tensor):
+            shape = shape.to(torch.int64)
         return input * torch.ones(
             torch.Size(shape), dtype=input.dtype, device=input.device
         )
