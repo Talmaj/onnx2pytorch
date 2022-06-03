ff8f6e8e91f69ef8d496215d918ca21d8baf75e1
Piyush Jha <piyush.jha@uwaterloo.ca>
2022-06-04 02:57:03 +0530

    Adding "value" argument as used in torch.nn.functional.pad

--- onnx2pytorch/operations/pad.py
+++ onnx2pytorch/operations/pad.py
@@ -4,17 +4,18 @@ from onnx2pytorch.operations.base import Operator
 
 
 class Pad(Operator):
-    def __init__(self, mode="constant", padding=None):
+    def __init__(self, mode="constant", padding=None, value=0):
         self.mode = mode
         self.padding = padding
+        self.value = value
         super().__init__()
 
-    def forward(self, input, pads=None, value=0):
+    def forward(self, input, pads=None):
         if self.padding is not None:
             pads = self.padding
         elif pads is None:
             raise TypeError("forward() missing 1 required positional argument: 'pads'")
-        out = F.pad(input, list(pads), mode=self.mode, value=value)
+        out = F.pad(input, list(pads), mode=self.mode, value=self.value)
         return out
 
     def extra_repr(self) -> str:
