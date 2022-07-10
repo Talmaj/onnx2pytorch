6f60cf657cd27ff97d4b94b9fce0651588a62f7d
Piyush Jha <piyush.jha@uwaterloo.ca>
2022-07-10 22:43:55 +0530

    Update pad.py

--- onnx2pytorch/operations/pad.py
+++ onnx2pytorch/operations/pad.py
@@ -17,7 +17,6 @@ class Pad(Operator):
             pads = self.padding
         elif pads is None:
             raise TypeError("forward() missing 1 required positional argument: 'pads'")
-        print(input.shape, pads.shape, value)
         out = F.pad(input, list(pads), mode=self.mode, value=value)
         return out
 
