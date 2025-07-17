Fixed dynamic calculation in LN

onnx2pytorch/operations/layernorm.py
@@ -4,9 +4,9 @@ from typing import Optional
 
 
 class LayerNorm(nn.Module):  # pylint: disable=missing-docstring
-    def __init__(self, axis: int, eps: float):
+    def __init__(self, normalized_shape: list, eps: float):
         super().__init__()
-        self.axis = axis
+        self.normalized_shape = normalized_shape
         self.eps = eps
 
     def forward(  # pylint: disable=missing-function-docstring
@@ -15,10 +15,9 @@ class LayerNorm(nn.Module):  # pylint: disable=missing-docstring
         scale: Optional[torch.Tensor] = None,
         bias: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
-        normalized_shape = inputs.shape[self.axis :]
         return nn.functional.layer_norm(
             input=inputs,
-            normalized_shape=normalized_shape,
+            normalized_shape=self.normalized_shape,
             weight=scale,
             bias=bias,
             eps=self.eps,