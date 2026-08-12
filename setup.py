c9313385cadd79ae129464f7b53a1010dd7ca16d
Talmaj Marinc <talmaj@gmail.com>
2026-08-12 15:44:12 +0200

    Drop support for python 3.10

--- setup.py
+++ setup.py
@@ -34,5 +34,5 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10",
+    python_requires=">=3.11",
 )
